071ebf70ef963d178a94f5efeefdc8fed3059066
AbdulH08 <hbbalga@gmail.com>
2025-02-24 08:36:28 +0700

    Update untitled14.py

--- untitled14.py
+++ untitled14.py
@@ -7,13 +7,11 @@ Original file is located at
     https://colab.research.google.com/drive/1lSc-cgrQQ2d1V6f43tMgmC4Chr7hypwB
 """
 
-import requests
+import gdown
+url = 'https://drive.google.com/uc?id=17tPbN2_7ZyBriedo7gHEB2KEYPE90rRs&export=download'  # Ganti dengan file ID Google Drive
+output = 'modelVGG16ep24.h5'
+gdown.download(url, output, quiet=False)
 
-url = "https://drive.google.com/uc?id=17tPbN2_7ZyBriedo7gHEB2KEYPE90rRs&export=download"
-response = requests.get(url)
-with open("output_file.ext", "wb") as f:
-    f.write(response.content)
-    
 import streamlit as st
 import tensorflow as tf
 import numpy as np
